7a8e7a25287096533e990e73d957fd4358a62ebd
Brubeckgolf <pclarke67@gmail.com>
2025-07-13 20:26:30 +0100

    Add files via upload

---
+++ Service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'golf-tracker-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+  // Add any CSS, JS, or other files here
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
